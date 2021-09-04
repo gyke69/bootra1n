36bca851526757e8a3a5395753ac272146b6490e
gyke69 <58794647+gyke69@users.noreply.github.com>
2021-09-04 02:23:53 -0700

    Update checkra1n.sh

--- checkra1n.sh
+++ checkra1n.sh
@@ -7,3 +7,6 @@
 wget https://assets.checkra.in/downloads/linux/cli/x86_64/dac9968939ea6e6bfbdedeb41d7e2579c4711dc2c5083f91dced66ca397dc51d/checkra1n
 chmod +x checkra1n
 sudo ./checkra1n
+
+esac
+done
