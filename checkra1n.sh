5936648b55b997151f6f4f3448a5bea424a7f7e1
gyke69 <58794647+gyke69@users.noreply.github.com>
2021-09-04 01:46:07 -0700

    Add files via upload

---
+++ checkra1n.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#This is a comment
+ 
+#defining a variable
+wget https://assets.checkra.in/downloads/linux/cli/x86_64/dac9968939ea6e6bfbdedeb41d7e2579c4711dc2c5083f91dced66ca397dc51d/checkra1n
+chmod +x checkra1n
+sudo ./checkra1n
