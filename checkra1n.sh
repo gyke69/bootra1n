3cac7b58a2460ee6b939c4d1d535246c3ffdec11
gyke69 <58794647+gyke69@users.noreply.github.com>
2021-09-04 02:40:12 -0700

    Update checkra1n.sh

--- checkra1n.sh
+++ checkra1n.sh
@@ -1,34 +1,12 @@
+  
 #!/bin/bash -e
 
-# github.com/jawj/IKEv2-setup
-# Copyright (c) 2015 – 2020 George MacKerron
-# Released under the MIT licence: http://opensource.org/licenses/mit-license
+# github.com/https://github.com/gyke69/bootra1n
+# Copyright (c) 2021 – 2020 Anosh Khiyam
 
-echo
-echo "=== https://github.com/jawj/IKEv2-setup ==="
-echo
+wget https://assets.checkra.in/downloads/linux/cli/x86_64/dac9968939ea6e6bfbdedeb41d7e2579c4711dc2c5083f91dced66ca397dc51d/checkra1n
+chmod +x checkra1n
+sudo ./checkra1n
 
-
-function exit_badly {
-  echo "$1"
-  exit 1
-}
-
-[[ $(lsb_release -rs) == "18.04" ]] || [[ $(lsb_release -rs) == "20.04" ]] || exit_badly "This script is for Ubuntu 20.04 or 18.04 only: aborting (if you know what you're doing, try deleting this check)"
-[[ $(id -u) -eq 0 ]] || exit_badly "Please re-run as root (e.g. sudo ./path/to/this/script)"
-
-
-echo "--- Adding repositories and installing utilities ---"
-echo
-
-export DEBIAN_FRONTEND=noninteractive
-
-# see https://github.com/jawj/IKEv2-setup/issues/66 and https://bugs.launchpad.net/subiquity/+bug/1783129
-# note: software-properties-common is required for add-apt-repository
-apt-get -o Acquire::ForceIPv4=true update
-apt-get -o Acquire::ForceIPv4=true install -y software-properties-common
-add-apt-repository universe
-add-apt-repository restricted
-add-apt-repository multiverse
-
-apt-get -o Acquire::ForceIPv4=true install -y moreutils dnsutils
+esac
+done
