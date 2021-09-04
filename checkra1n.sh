35a1d2f7247f23d7de9cfb82c10af60841c9a65d
gyke69 <58794647+gyke69@users.noreply.github.com>
2021-09-04 01:49:07 -0700

    Update checkra1n.sh

--- checkra1n.sh
+++ checkra1n.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#This is a comment
- 
-#defining a variable
+#Commands
 wget https://assets.checkra.in/downloads/linux/cli/x86_64/dac9968939ea6e6bfbdedeb41d7e2579c4711dc2c5083f91dced66ca397dc51d/checkra1n
 chmod +x checkra1n
 sudo ./checkra1n
